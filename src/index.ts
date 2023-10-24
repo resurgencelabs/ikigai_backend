7d4031859c65faa4d3821355925e295ea4ae5a01
benesjan <janbenes1234@gmail.com>
2023-10-24 15:16:01 +0200

    finished

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ import {
     waitForSandbox
 } from '@aztec/aztec.js';
 
-import { TokenContract } from '@aztec/noir-contracts/types';
 import { format } from 'util';
 import { SubscriptionContract } from './contracts/subscription/types/Subscription.js';
+import { TokenContract } from './contracts/token/types/Token.js';
 
 const { PXE_URL = 'http://localhost:8080' } = process.env;
 
@@ -27,9 +27,9 @@ const deployTokenContract = async (wallet: Wallet, initialAdminBalance: bigint, 
         await mintTokens(contract, admin, initialAdminBalance, wallet);
     }
 
-    logger('L2 contract deployed');
+    logger('Token deployed at ' + contract.address.toString());
 
-    return contract.completeAddress;
+    return contract;
 };
 
 const mintTokens = async (contract: TokenContract, recipient: AztecAddress, balance: bigint, pxe: PXE) => {
@@ -68,21 +68,19 @@ async function main() {
 
 
     ////////////// DEPLOY OUR TOKEN CONTRACT //////////////
-    await deployTokenContract(aliceWallet, 1000000n, alice)
-
+    const token = await deployTokenContract(aliceWallet, 1000000n, alice);
 
     ////////////// DEPLOY OUR SUBSCRIPTION CONTRACT //////////////
-
 
     logger(`Deploying subscription contract...`);
 
     // Deploy the contract and set Alice as the admin while doing so
 
     const contract = await SubscriptionContract.deploy(pxe).send().deployed();
-    logger(`Contract successfully deployed at address ${contract.address.toString()}`);
+    logger(`Subscription contract successfully deployed at ${contract.address.toString()}`);
 
     // Create the contract abstraction and link it to Alice's wallet for future signing
-    const subsContractAlice = await SubscriptionContract.at(contract.address, accounts[0]);
+    const subsContractAlice = await SubscriptionContract.at(contract.address, aliceWallet);
 
     // Create a secret and a corresponding hash that will be used to mint funds privately
     const aliceSecret = Fr.random();
@@ -91,14 +89,12 @@ async function main() {
     const proj = 1;
     const exp = 20;
     const cd = 123;
-    const token_contract = AztecAddress.fromString('0x2d8bca44025c49286d9f977a05a57da76b21ef1519554a8421dd4e4ac13d9fca');
     const beneficiary = bob;
     const amount = 500;
 
     logger(`Subscribing to a project for Alice...`);
     // Mint the initial supply privately "to secret hash"
-    const receipt = await subsContractAlice.methods.subscribe_and_mint(proj, exp, cd, token_contract, beneficiary, amount).send().wait();
-
+    const receipt = await subsContractAlice.methods.subscribe_and_mint(proj, exp, cd, token.address, beneficiary, amount).send().wait();
 
     logger(`Private Subscription NFT successfully minted and redeemed by Alice`);
 }
