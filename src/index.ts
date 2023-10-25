a758bc5b0fc31e836df8c8c92a9a56f4289d4cba
benesjan <janbenes1234@gmail.com>
2023-10-25 13:57:17 +0200

    updated versions

--- src/index.ts
+++ src/index.ts
@@ -76,7 +76,7 @@ async function main() {
 
     // Deploy the contract and set Alice as the admin while doing so
 
-    const contract = await SubscriptionContract.deploy(pxe).send().deployed();
+    const contract = await SubscriptionContract.deploy(aliceWallet).send().deployed();
     logger(`Subscription contract successfully deployed at ${contract.address.toString()}`);
 
     // Create the contract abstraction and link it to Alice's wallet for future signing
